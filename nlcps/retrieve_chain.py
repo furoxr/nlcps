98e384ef143aeee7e5f64d00a41105a19a7dbc77
nada <37610161+furoxr@users.noreply.github.com>
2023-08-20 21:35:21 +0800

    feat: refactor retrieve chain

--- nlcps/retrieve_chain.py
+++ nlcps/retrieve_chain.py
@@ -7,6 +7,7 @@ from langchain.prompts import (
     FewShotChatMessagePromptTemplate,
 )
 from langchain.chat_models import ChatOpenAI
+from pydantic import BaseModel
 from qdrant_client.models import Filter, FieldCondition, MatchValue, MatchAny
 from nlcps.selector import FilterExampleSelector
 from nlcps.types import (
@@ -16,7 +17,7 @@ from nlcps.types import (
     RetrieveExample,
 )
 
-from typing import List, Optional
+from typing import Any, List, Optional
 
 
 RETRIEVE_PROMPT = (
@@ -27,61 +28,22 @@ RETRIEVE_PROMPT = (
 )
 
 
-class RetrieveChain:
-    def __init__(
-        self,
-        llm: ChatOpenAI,
-        user_utterance: str,
-        system_instruction: str,
-        analysis_result: AnalysisResult,
-        dsl_syntax_selector: FilterExampleSelector[DSLSyntaxExample],
-        dsl_rules_selector: FilterExampleSelector[DSLRuleExample],
-        dsl_examples_selector: FilterExampleSelector[RetrieveExample],
-        context: Optional[str] = None,
-    ) -> None:
-        """Initialize an analysis chain.
+class RetrieveChain(BaseModel):
+    llm: ChatOpenAI
+    system_instruction: str
+    dsl_syntax_selector: FilterExampleSelector[DSLSyntaxExample]
+    dsl_rules_selector: FilterExampleSelector[DSLRuleExample]
+    dsl_examples_selector: FilterExampleSelector[RetrieveExample]
 
-        This chain will analyze user utterance and returns a AnalysisResult.
-
-        Args:
-            llm (ChatOpenAI): Interface with openai
-            entities (List[Entity]): All entities DSL supported
-            context_rules (List[str]): Rules inster into prompt, helping AI decide whether context is needed
-            examples (List[AnalysisExample]): Few-shot prompt examples
-        """
-        few_shot_prompt = self.few_shot_exmaple_template(
-            user_utterance, analysis_result.entities, dsl_examples_selector
-        )
-        self.prompt_template = ChatPromptTemplate.from_messages(
-            [
-                SystemMessagePromptTemplate.from_template(RETRIEVE_PROMPT),
-                few_shot_prompt,
-                HumanMessagePromptTemplate.from_template("{input}\n{context}"),
-            ]
-        )
-        self.prompt_template = self.prompt_template.partial(
-            system_instruction=system_instruction
-        )
-        self.format_dsl_syntax(dsl_syntax_selector, analysis_result.entities)
-        self.format_dsl_rules(dsl_rules_selector, analysis_result.entities)
-        if context:
-            self.prompt_template = self.prompt_template.partial(
-                context=f"Context is:\n{context}"
-            )
-
-        self.chain = LLMChain(
-            llm=llm,
-            prompt=self.prompt_template,
-        )
+    prompt_template: Any = None
 
     def format_dsl_syntax(
         self,
-        dsl_syntax_selector: FilterExampleSelector[DSLSyntaxExample],
         entities: List[str],
     ):
         """Format all DSL syntax code examples of which entities is a subset of entities"""
-        key = f"{dsl_syntax_selector.qdrant.metadata_payload_key}.entities"
-        dsl_syntax_examples = dsl_syntax_selector.select(
+        key = f"{self.dsl_syntax_selector.qdrant.metadata_payload_key}.entities"
+        dsl_syntax_examples = self.dsl_syntax_selector.select(
             Filter(must=[FieldCondition(key=key, match=MatchAny(any=entities))])
         )
         self.prompt_template = self.prompt_template.partial(
@@ -90,12 +52,11 @@ class RetrieveChain:
 
     def format_dsl_rules(
         self,
-        dsl_rules_selector: FilterExampleSelector[DSLRuleExample],
         entities: List[str],
     ):
         """Format all DSL rules related to entities"""
-        key = f"{dsl_rules_selector.qdrant.metadata_payload_key}.entities"
-        dsl_rules_examples = dsl_rules_selector.select(
+        key = f"{self.dsl_rules_selector.qdrant.metadata_payload_key}.entities"
+        dsl_rules_examples = self.dsl_rules_selector.select(
             Filter(must=[FieldCondition(key=key, match=MatchAny(any=entities))])
         )
         self.prompt_template = self.prompt_template.partial(
@@ -106,24 +67,23 @@ class RetrieveChain:
         self,
         user_utterance: str,
         entities: List[str],
-        dsl_examples_selector: FilterExampleSelector[RetrieveExample],
     ) -> FewShotChatMessagePromptTemplate:
         """Get all examples of this DSL related to user utterance"""
-        key = f"{dsl_examples_selector.qdrant.metadata_payload_key}.entities"
+        key = f"{self.dsl_examples_selector.qdrant.metadata_payload_key}.entities"
         condition = Filter(
             must=[
                 FieldCondition(key=key, match=MatchValue(value=entity))
                 for entity in entities
             ]
         )
-        similarity_examples = dsl_examples_selector.similarity_select(
+        similarity_examples = self.dsl_examples_selector.similarity_select(
             {"user_utterance": user_utterance}, filter=condition
         )
         similarity_examples.sort(key=lambda x: x[1])
         similarity_examples.reverse()
 
         final_examples = []
-        for index in range(max([dsl_examples_selector.k, len(entities)])):
+        for index in range(max([self.dsl_examples_selector.k, len(entities)])):
             example = similarity_examples[index][0]
             final_examples.append(
                 {
@@ -146,5 +106,34 @@ class RetrieveChain:
         )
         return few_shot_prompt
 
-    def run(self, user_utterance: str) -> str:
-        return self.chain.run(user_utterance)
+    def init_chain(
+        self, user_utterance: str, entities: List[str], context: Optional[str] = None
+    ):
+        few_shot_prompt = self.few_shot_exmaple_template(user_utterance, entities)
+        self.prompt_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessagePromptTemplate.from_template(RETRIEVE_PROMPT),
+                few_shot_prompt,
+                HumanMessagePromptTemplate.from_template("{input}\n{context}"),
+            ]
+        )
+        self.prompt_template = self.prompt_template.partial(
+            system_instruction=self.system_instruction
+        )
+        self.format_dsl_syntax(entities)
+        self.format_dsl_rules(entities)
+        if context:
+            self.prompt_template = self.prompt_template.partial(
+                context=f"Context is:\n{context}"
+            )
+
+        self.chain = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt_template,
+        )
+
+    def run(
+        self, user_utterance: str, entities: List[str], context: Optional[str] = None
+    ) -> str:
+        self.init_chain(user_utterance, entities, context)
+        return self.chain.run(input=user_utterance, context=context)
